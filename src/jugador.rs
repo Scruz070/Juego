8be3f18394a58c1153b36797e9dc0008d39bfcd7
Scruz070 <147681327+Scruz070@users.noreply.github.com>
2024-10-08 09:43:41 -0600

    Steven

--- src/jugador.rs
+++ src/jugador.rs
@@ -1,14 +1,22 @@
+// Definición de la estructura `JugadorNBA` que representa a un jugador de la NBA
 pub struct JugadorNBA {
-    nombre: String,
-    posicion: String,
-    equipo: String,    
-    partidos_jugados: i8,
-    minutos_por_juego: f32,
-    puntos_por_juego: f32,
+    nombre: String,              // Nombre del jugador
+    posicion: String,            // Posición en la que juega
+    equipo: String,              // Equipo en el que juega
+    partidos_jugados: i8,        // Número de partidos jugados
+    minutos_por_juego: f32,      // Promedio de minutos jugados por partido
+    puntos_por_juego: f32,       // Promedio de puntos anotados por partido
 }
 
-pub fn crear_jugador(nombre: String, posicion: String, equipo: String, partidos_jugados: i8, 
-                 minutos_por_juego: f32, puntos_por_juego: f32) -> JugadorNBA {
+// Función que crea y retorna un nuevo jugador de la NBA
+pub fn crear_jugador(
+    nombre: String, 
+    posicion: String, 
+    equipo: String, 
+    partidos_jugados: i8, 
+    minutos_por_juego: f32, 
+    puntos_por_juego: f32
+) -> JugadorNBA {
     JugadorNBA {
         nombre,
         posicion,
@@ -19,35 +27,42 @@ pub fn crear_jugador(nombre: String, posicion: String, equipo: String, partidos_
     }
 }
 
+// Función que imprime los detalles de un jugador dado
 pub fn imprimir_jugador(jugador: &JugadorNBA) {
     println!("\n*** Información del jugador ***");
-    println!("Nombre: {}", jugador.nombre);
-    println!("Equipo: {}", jugador.equipo);
-    println!("Posición: {}", jugador.posicion);    
-    println!("Total partidos jugados: {}", jugador.partidos_jugados);
-    println!("Promedio de minutos por partido: {}", jugador.minutos_por_juego);
-    println!("Promedio de puntos por partido: {}", jugador.puntos_por_juego);
+    println!("Nombre: {}", jugador.nombre);                   // Imprime el nombre del jugador
+    println!("Equipo: {}", jugador.equipo);                   // Imprime el equipo
+    println!("Posición: {}", jugador.posicion);               // Imprime la posición
+    println!("Total partidos jugados: {}", jugador.partidos_jugados);  // Imprime la cantidad de partidos jugados
+    println!("Promedio de minutos por partido: {}", jugador.minutos_por_juego);  // Imprime el promedio de minutos por partido
+    println!("Promedio de puntos por partido: {}", jugador.puntos_por_juego);    // Imprime el promedio de puntos por partido
 }
 
+// Función que busca un jugador en una lista por su nombre
 pub fn buscar_jugador<'a>(jugadores: &'a [JugadorNBA], nombre_buscar: &str) -> Option<&'a JugadorNBA> {
+    // Itera sobre los jugadores en la lista
     for jugador in jugadores {
+        // Si encuentra un jugador con el nombre buscado, lo retorna
         if jugador.nombre == nombre_buscar {
             return Some(jugador);
         }
     }
-    None
+    None  // Si no lo encuentra, retorna `None`
 }
 
+// Función que busca el jugador con más puntos por partido
 pub fn buscar_jugador_mas_puntos(jugadores: &[JugadorNBA]) -> Option<&JugadorNBA> {
-    let mut max_puntos_por_juego = 0.0;
-    let mut jugador_mas_puntos: Option<&JugadorNBA> = None;
+    let mut max_puntos_por_juego = 0.0;                // Inicializa el máximo de puntos con 0
+    let mut jugador_mas_puntos: Option<&JugadorNBA> = None;  // Inicializa la variable del jugador con más puntos
 
+    // Itera sobre todos los jugadores
     for jugador in jugadores {
+        // Si encuentra un jugador con un promedio de puntos mayor al máximo actual
         if jugador.puntos_por_juego > max_puntos_por_juego {
-            max_puntos_por_juego = jugador.puntos_por_juego;
-            jugador_mas_puntos = Some(jugador);
+            max_puntos_por_juego = jugador.puntos_por_juego;  // Actualiza el máximo de puntos
+            jugador_mas_puntos = Some(jugador);               // Actualiza el jugador con más puntos
         }
     }
 
-    jugador_mas_puntos
+    jugador_mas_puntos  // Retorna el jugador con más puntos, o `None` si no hay jugadores
 }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,43 @@
 use std::io;
 // Importar el módulo jugador.rs
-mod jugador; 
+mod jugador;
 
 // Importar los elementos necesarios del módulo jugador.rs
-use jugador::{JugadorNBA, crear_jugador, imprimir_jugador, buscar_jugador, buscar_jugador_mas_puntos}; 
+use jugador::{JugadorNBA, crear_jugador, imprimir_jugador, buscar_jugador, buscar_jugador_mas_puntos};
 
 fn main() { 
+    // Crear instancias de JugadorNBA utilizando la función `crear_jugador`
     let jugador1 = crear_jugador(String::from("LeBron James"),
-    String::from("Alero"),
-    String::from("Los Angeles Lakers"),
-    60, 34.7, 25.0);
+        String::from("Alero"),
+        String::from("Los Angeles Lakers"),
+        60, 34.7, 25.0);
 
     let jugador2 = crear_jugador(String::from("Stephen Curry"),
-    String::from("Base"),
-    String::from("Golden State Warriors"),
-    58, 33.2, 29.5);
+        String::from("Base"),
+        String::from("Golden State Warriors"),
+        58, 33.2, 29.5);
 
     let jugador3 = crear_jugador(String::from("Kevin Durant"),
-    String::from("Alero"),
-    String::from("Brooklyn Nets"),
-    45,35.6, 27.3);
+        String::from("Alero"),
+        String::from("Brooklyn Nets"),
+        45, 35.6, 27.3);
 
     let jugador4 = crear_jugador(String::from("Giannis Antetokounmpo"),
-    String::from("Alero"),
-    String::from("Milwaukee Bucks"),
-    62, 36.1, 28.5);
+        String::from("Alero"),
+        String::from("Milwaukee Bucks"),
+        62, 36.1, 28.5);
 
     let jugador5 = crear_jugador(String::from("Luka Dončić"),
-    String::from("Base"),
-    String::from("Dallas Mavericks"),
-    56, 35.2, 26.9);        
+        String::from("Base"),
+        String::from("Dallas Mavericks"),
+        56, 35.2, 26.9);        
 
     // Crear un arreglo de JugadorNBA
     let arreglo_jugadores = [jugador1, jugador2, jugador3, jugador4, jugador5];
 
+    // Ciclo que maneja el menú de la aplicación
     loop {
+        // Mostrar el menú de opciones
         println!("\n\nMenú de la aplicación");
         println!("---------------------");
         println!("1. Listar todos los jugadores");
@@ -43,44 +46,52 @@ fn main() {
         println!("4. Salir");
         println!("Ingrese una opción:");
 
+        // Leer la opción del usuario
         let mut opcion = String::new();
         io::stdin()
             .read_line(&mut opcion)
             .expect("Error al leer la entrada");
 
+        // Evaluar la opción ingresada
         match opcion.trim() {
             "1" => {                
+                // Opción 1: Listar todos los jugadores
                 println!("\n-> Jugadores registrados en la aplicación");
                 for jugador in &arreglo_jugadores {
-                    imprimir_jugador(&jugador);
+                    imprimir_jugador(&jugador);  // Imprimir los datos de cada jugador
                 }
             }
             "2" => {                
-                println!("\n-> Buscar jugador por nombre");                
-                let nombre_buscar = String::from("Giannis Antetokounmpo");         
+                // Opción 2: Buscar un jugador por su nombre
+                println!("\n-> Buscar jugador por nombre");
+                let nombre_buscar = String::from("Giannis Antetokounmpo");  // Nombre a buscar
                 let jugador_encontrado = buscar_jugador(&arreglo_jugadores, &nombre_buscar.trim());
 
+                // Verificar si se encontró el jugador
                 match jugador_encontrado {
                     Some(jugador) => {
-                    println!("\n*** Jugador encontrado: ***");
-                    imprimir_jugador(jugador);
-                },
-                None => println!("\n -> No se encontró ningún jugador con ese nombre."),
+                        println!("\n*** Jugador encontrado: ***");
+                        imprimir_jugador(jugador);  // Imprimir la información del jugador encontrado
+                    },
+                    None => println!("\n -> No se encontró ningún jugador con ese nombre."),
                 }
             }
             "3" => {
+                // Opción 3: Buscar el jugador con el mayor promedio de puntos
                 if let Some(jugador) = buscar_jugador_mas_puntos(&arreglo_jugadores) {
                     println!("\n-> Jugador con más puntos por juego:");
-                    imprimir_jugador(jugador);
+                    imprimir_jugador(jugador);  // Imprimir los datos del jugador con más puntos
                 } else {
                     println!("\n -> No se encontró ningún jugador.");
                 }
             }
             "4" => {
+                // Opción 4: Salir de la aplicación
                 println!("*** Saliendo de la aplicación *** ");
-                break;
+                break;  // Salir del bucle
             }
             _ => {
+                // Opción inválida: mostrar mensaje de error
                 println!("-> Opción inválida. Por favor, ingrese una opción válida.");
             }
         }
